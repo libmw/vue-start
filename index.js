17fc207f48671bb2a78ddae0d130f4becde8bf3d
limengjun <libmw@163.com>
2019-11-28 16:06:57 +0800

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express')
+const cookieParser = require('cookie-parser')
+
+var app = express()
+app.use(cookieParser())
+app.all('/', function (req, res) {
+        res.set({
+            'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+            'Access-Control-Allow-Headers': 'X-PINGOTHER',
+            'Access-Control-Allow-Origin': 'http://127.0.0.1:4000',
+            'ETag': '12345'
+
+        })
+    //res.cookie('rememberme', '1', { expires: new Date(Date.now() + 900000)})
+    res.send('hello world')
+    console.log('get request', new Date(), 'cookie:', req.cookies.rememberme);
+})
+app.listen(3000)
